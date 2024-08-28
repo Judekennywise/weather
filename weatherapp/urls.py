65118c53ccb32d8ea187d396777c9b7c7d6614f7
Jude <judeokennywise@gmail.com>
2024-08-28 14:15:36 +0100

    update

--- weatherapp/urls.py
+++ weatherapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup_view, name='signup'),
-    path('login/', views.login_view, name='login'),
+    path('accounts/login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('profile/', views.profile_view, name='profile'),
